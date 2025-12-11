acb4f1821ba709689fcc10a8b0ae4e83a5ceb7fb
kaya <kaya_frog_ramer@kakao.com>
2025-12-12 02:26:11 +0900

    feat: Book, Category 모델 정의

---
+++ backend/books/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=150)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=20)
+    page = models.IntegerField()
+    thumbnail = models.URLField(max_length=500)
+    category = models.ForeignKey(
+        to=Category,
+        on_delete=models.PROTECT,
+        related_name='books',
+    )
+
+    def __str__(self):
+        return self.title
+    
