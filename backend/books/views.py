2b2af05a1534a9eb6f51a2cf0b33330e62d529b6
kayaaaaaaaaaaa <kaya_frog_ramer@kakao.com>
2025-12-14 16:54:09 +0900

    feat: Book 도메인 엔드포인트 정의

--- backend/books/views.py
+++ backend/books/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+def detail(request, book_id):
+    pass
+
+def create(request):
+    pass
+
+def review_list_and_create(request, book_id):
+    pass
+
+def galfy_list_and_create(request, book_id):
+    pass
