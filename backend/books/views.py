1042e32526e2c77023f2afa921b6265154c99fbd
kayaaaaaaaaaaa <kaya_frog_ramer@kakao.com>
2025-12-14 17:08:33 +0900

    feat: Book 상세정보 조회 기능 추가

--- backend/books/views.py
+++ backend/books/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
+@api_view(['GET'])
 def detail(request, book_id):
-    pass
+    book = get_object_or_404(Book, id=book_id)
+    serializer = BookSerializer(book)
+    return Response(serializer.data)
 
 def create(request):
     pass
