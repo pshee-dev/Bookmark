d43c4dfef2e45069d9fb76b9b07d107ee19d8c72
kaya <kaya_frog_ramer@kakao.com>
2025-12-16 00:48:12 +0900

    refactor: 따옴표 통일

--- backend/books/serializers.py
+++ backend/books/serializers.py
@@ -4,14 +4,14 @@ from .models import Book, Category
 class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
-        fields = ["id", 'name']
+        fields = ['id', 'name']
         read_only_fields = ["id"]
 
 class BookSerializer(ModelSerializer):
     category = CategorySerializer(read_only=True)
     class Meta:
         model = Book
-        fields = ["id", 'title', 'author', 'publisher', 'published_date', 'isbn', 'page', 'thumbnail', 'category']
+        fields = ['id', 'title', 'author', 'publisher', 'published_date', 'isbn', 'page', 'thumbnail', 'category']
         read_only_fields = ["id"]
     #TODO 유효성검사 로직 필요
     
