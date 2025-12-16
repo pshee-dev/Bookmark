6737991a37b16620fd32674f6de978f5221a2501
박설희 <pshee.dev@gmail.com>
2025-12-16 14:39:41 +0900

    refactor: urls.py 구체적인 경로 상단 배치

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:user_id>/', views.profile),
     path('<int:user_id>/follow/', views.follow),
     path('<int:user_id>/followings/', views.get_following_list),
     path('<int:user_id>/followers/', views.get_follower_list),
     path('<int:user_id>/reviews/', views.get_review_list),
     path('<int:user_id>/galfies/', views.get_galfy_list),
     path('<int:user_id>/feed/', views.get_feed),
+    path('<int:user_id>/', views.profile),
 ]
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -25,8 +25,8 @@ import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),    
-    path('api/v1/accounts/', include('dj_rest_auth.urls')),
     path('api/v1/accounts/signup/', include('dj_rest_auth.registration.urls')),
+    path('api/v1/accounts/', include('dj_rest_auth.urls')),
     path('api/v1/users/', include('accounts.urls')), # 인증 관련 경로보다 아래에 위치해야함
     path('api/v1/books/', include('books.urls', namespace='books'), name='books'), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
