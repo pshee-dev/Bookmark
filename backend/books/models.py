f3a6a328f08b376d50117cea7e6cdbb3e106011b
kaya <kaya_frog_ramer@kakao.com>
2025-12-17 08:33:33 +0900

    feat: 카테고리 매핑 전용 모델 ExternalCategoryMapping 정의

--- backend/books/models.py
+++ backend/books/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
-    
+
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=100, blank=True)
@@ -22,4 +22,20 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+#TODO 복합유니크 걸어야함 
+class ExternalCategoryMapping(models.Model):
+    provider = models.CharField(max_length=30, blank=True)  # 알라딘
+    external_cid = models.CharField(max_length=50, blank=True)
+    external_name = models.CharField(max_length=50, blank=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        related_name='external_mappings',
+    )
     
+    def __str__(self):
+        return f"cid: {self.external_cid}, Category: {self.category.name}"
+    
+    class Meta:
+        db_table = 'books_external_category_mapping'
