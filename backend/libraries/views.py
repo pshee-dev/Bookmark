d9dd231891ed82944d08df68c0b8b0237aae1379
박설희 <pshee.dev@gmail.com>
2025-12-17 23:58:52 +0900

    feat: 서재 독서 상태 삭제 기능 구현

--- backend/libraries/views.py
+++ backend/libraries/views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
 from rest_framework.response import Response
 from django.shortcuts import get_list_or_404, get_object_or_404
@@ -6,6 +7,7 @@ from .serializers import LibraryBookListSerializer, LibraryBookCreateSerializer,
 from .models import Library
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def library_book_list(request):
     # 내 서재 도서 목록 조회
     if request.method == 'GET':
@@ -22,8 +24,10 @@ def library_book_list(request):
         
 
 @api_view(['GET', 'PATCH', 'DELETE'])
+@permission_classes([IsAuthenticated])
 def library_book(request, library_id):
-    library = get_object_or_404(Library, pk=library_id)
+    # 현재 로그인된 유저의 library 객체에만 접근 가능하도록 설정하여 타 유저의 서재에 있는 도서에 대한 상세 조회/수정/삭제 접근을 막음
+    library = get_object_or_404(Library, pk=library_id, user=request.user)
 
     # 독서 상태 상세 조회
     if request.method == 'GET':
@@ -38,4 +42,5 @@ def library_book(request, library_id):
 
     # 독서 상태 삭제
     elif request.method == 'DELETE':
-        pass
+        library.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
