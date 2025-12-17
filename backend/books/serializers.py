b84ce90e98ad19b6ac14124b45ba5446d98ed89a
kaya <kaya_frog_ramer@kakao.com>
2025-12-18 00:47:00 +0900

    refacto: 따옴표 통일

--- backend/books/serializers.py
+++ backend/books/serializers.py
@@ -5,11 +5,11 @@ class CategorySerializer(ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name']
-        read_only_fields = ["id"]
+        read_only_fields = ['id']
 
 class BookSerializer(ModelSerializer):
     category_id = PrimaryKeyRelatedField(
-        source="category",
+        source='category',
         queryset=Category.objects.all(),
         write_only=True
     )
@@ -20,6 +20,6 @@ class BookSerializer(ModelSerializer):
             'id', 'title', 'author', 'publisher', 'published_date', 
             'isbn', 'page', 'thumbnail', 'category_id', 'category'
         ]
-        read_only_fields = ["id"]
+        read_only_fields = ['id']
     #TODO 유효성검사 로직 필요
     
