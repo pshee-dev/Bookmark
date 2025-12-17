6fa0387fccd91b2f35e281a1a4b14888de6eb95d
kaya <kaya_frog_ramer@kakao.com>
2025-12-18 00:46:46 +0900

    feat: 리뷰 단건 시리얼라이저 추가

---
+++ backend/reviews/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Review
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = [
+            'id',
+            'title',
+            'content',
+            'created_at',
+            'updated_at',
+            'user',
+            'book',
+        ]
+        read_only_fields = ['id', 'created_at', 'updated_at', 'user']
