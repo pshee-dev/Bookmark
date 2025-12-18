6ad419ae5023ffc4dddb6e3c088deb84cc5cabae
kaya <kaya_frog_ramer@kakao.com>
2025-12-19 00:54:53 +0900

    refactor: 리뷰, 갈피 모델 __str__() 함수에 들어가는 작성자 항목을 username->풀네임으로 변경

--- backend/reviews/models.py
+++ backend/reviews/models.py
@@ -17,5 +17,13 @@ class Review(models.Model):
                              related_name='reviews',
                              )
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "book"], # 한 유저는 같은 책에 하나의 리뷰만 작성 가능
+                name="unique_review_per_user_per_book", # 마이그레이션 시 식별용
+            )
+        ]
+
     def __str__(self):
-        return f"\n 도서 이름: {self.book}\n리뷰 제목: {self.title}\n작성자: {self.user.username} "
+        return f"\n 도서 이름: {self.book}\n리뷰 제목: {self.title}\n작성자: f'{self.user.last_name}{self.user.first_name}' "
