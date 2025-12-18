d0d97edcbfaab140924900c36742015d1c3ba5a2
kaya <kaya_frog_ramer@kakao.com>
2025-12-18 21:39:03 +0900

    refactor: 리뷰가 연관객체 요약정보도 포함해서 반환하도록 연관객체 필드 정의 방식 변경

--- backend/reviews/serializers.py
+++ backend/reviews/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
+from rest_framework.relations import PrimaryKeyRelatedField
+
 from .models import Review
+from accounts.serializers import UserProfileSerializer
+from books.serializers import BookSummarySerializer
 
 class ReviewSerializer(serializers.ModelSerializer):
+    book = BookSummarySerializer(read_only=True)
+    user = UserProfileSerializer(read_only=True)
     class Meta:
         model = Review
         fields = [
@@ -12,8 +18,11 @@ class ReviewSerializer(serializers.ModelSerializer):
             'updated_at',
             'user',
             'book',
+            # TODO 좋아요 개수 추가
+            # TODO 서재 평점 추가
+            # TODO 댓글 개수 추가
         ]
-        read_only_fields = ['id', 'created_at', 'updated_at', 'user']
+        read_only_fields = ['id', 'created_at', 'updated_at', 'user', 'book']
 
 class ReviewCreateSerializer(serializers.ModelSerializer):
     class Meta:
