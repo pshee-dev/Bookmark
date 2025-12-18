74e55d7b9554129186543a0c485ea3d46d24e218
박설희 <pshee.dev@gmail.com>
2025-12-19 00:31:47 +0900

    refactor: User 모델 full_name 속성 추가

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -11,3 +11,7 @@ class User(AbstractUser):
 
     def __str__(self):
         return f'{self.last_name}{self.first_name}'
+    
+    @property
+    def full_name(self):
+        return f'{self.last_name}{self.first_name}'
