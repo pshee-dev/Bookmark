4a1e68933e96567e704735a71be23c0395c46a2e
kayaaaaaaaaaaa <kaya_frog_ramer@kakao.com>
2025-12-19 17:45:57 +0900

    fix: 도서 예외 핸들러 누락된 속성 추가

--- backend/books/errors.py
+++ backend/books/errors.py
@@ -4,11 +4,13 @@ class BookExceptionHandler(Exception):
     user_message = "현재 서비스 이용이 불가하오니, 나중에 다시 시도해 주세요."
     http_status = 400
 
-    def __init__(self, *, dev_message=None, cause=None):
+    def __init__(self, *, dev_message=None, cause=None, user_message=None):
         self.dev_message = dev_message
         self.cause = cause
         if self.dev_message: # 로거가 없으므로 에러찾기용으로 임시적용
             print(self.dev_message)
+        if user_message:
+            self.user_message=user_message
         super().__init__(dev_message)
 
 class InvalidQuery(BookExceptionHandler):
