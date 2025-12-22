8861cc9431b760c4253487be18bc0efc21d1b777
박설희 <pshee.dev@gmail.com>
2025-12-22 17:30:43 +0900

    feat: 갈피/리뷰 카드 컴포넌트 구조 및 스타일링

--- frontend/src/components/feed/FeedBase.vue
+++ frontend/src/components/feed/FeedBase.vue
@@ -1,17 +1,132 @@
 <script setup>
+  import { computed } from 'vue'
 
-const props = defineProps({
-  feed: Object,
-  feedType: String,
-})
+  const props = defineProps({
+    feed: Object,
+    feedType: String,
+  })
 
+  const formattedDate = computed(() => {
+    return props.feed.created_at?.slice(0, 10)
+  })
 
+  const goFeedDetail = () => {
+
+  }
 </script>
 
 <template>
-  
+  <div class="feed-card" @click.stop="goFeedDetail">
+    <div class="card-profile">
+      <div class="profile-image">
+        <img v-if="feed.user.profile_img" :src="feed.user.profile_img" :alt="feed.user.full_name">
+        <img v-else src="@/assets/images/no_img_profile.png" alt="profile-no-image">
+      </div>
+      <p class="name">{{ feed.user.full_name }}</p>
+    </div>
+    <hr class="line">
+    <div class="card-body">
+      <div class="tit-box">
+        <h3 v-if="feedType === 'review'" class="title">{{ feed.title }}</h3>
+        <h3 v-if="feedType === 'galfy'" class="title">P. {{ feed.page_number }}</h3>
+        <span class="date">{{ formattedDate }}</span>
+      </div>
+      <p class="content">{{ feed.content }}</p>
+    </div>
+    <div class="actions">
+      <button class="btn-action like">
+        <img src="@/assets/images/common/icon_like.png" alt="like">
+        <!-- {{ feed.likeCount }} -->
+      </button>
+      <button class="btn-action comment">
+        <img src="@/assets/images/common/icon_comment.png" alt="comment">
+        <!-- {{ feed.commentCount }} -->
+      </button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 
+  .feed-card {
+    background: linear-gradient(90deg, rgba(69, 106, 255, 0.05) 0%, rgba(163, 58, 255, 0.05) 100%);
+    padding: 30px 40px 50px;
+    border-radius: 20px;
+    transition-duration: .2s;
+    border: 2px solid transparent;
+    cursor: pointer;
+  }
+
+  .feed-card:hover {
+    border: 2px solid #7830b7;
+  }
+
+  .card-profile {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 10px;
+  }
+
+  .profile-image {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    overflow: hidden;
+  }
+
+  .profile-image img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  .name {
+    font-size: 18px;
+    font-weight: 600;
+  }
+
+  .line {
+    margin: 20px 0 30px;
+    border: 1px soild #ccc;
+  }
+
+  .card-body {
+    margin-bottom: 40px;
+  }
+
+  .tit-box {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 18px;
+  }
+
+  .title {
+    font-size: 20px;
+    color: #333;
+  }
+
+  .date {
+    font-size: 16px;
+    color: #767676;
+  }
+
+  .content {
+    font-size: 18px;
+    color: #555;
+  }
+
+  .actions {
+    display: flex;
+    gap: 25px;
+    align-items: center;
+    justify-content: flex-start;
+  }
+
+  .btn-action {
+    outline: none;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+  }
 </style>
